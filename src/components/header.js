0f78829381458b078d515bd2512f896900c7107b
Portia <plburton@gmail.com>
2020-01-03 17:01:29 +0100

    Changed background color of header

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#5B3284`,
       marginBottom: `1.45rem`,
     }}
   >
